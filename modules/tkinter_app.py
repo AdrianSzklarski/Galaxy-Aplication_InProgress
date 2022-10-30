23e40390cc15c5b9a6bfe9f3959e461b92fe40f3
AdrianSzklarski <adrian_it@o2.pl>
2022-10-30 23:16:12 +0100

    Added full screen window & name of title bar

--- modules/tkinter_app.py
+++ modules/tkinter_app.py
@@ -1,10 +1,23 @@
-
 class MainCodeOfApp:
     def __init__(self, root):
         """
 
         @rtype: object
         """
+        self.frame = None
         self.root = root
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+
+        # calling modules
+        self.get_settings_window()
+
+    def get_settings_window(self):
+        """
+
+        @rtype: object
+        """
+        self.root.title("Task Application by Adrian Szklarski, 2022")
+        self.root.geometry(f"{self.screen_width}x{self.screen_height}")
 
 
