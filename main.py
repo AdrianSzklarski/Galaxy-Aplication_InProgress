3255d5ed0c14e8e888d7537fe3d0fcaafd3885d0
AdrianSzklarski <adrian_it@o2.pl>
2022-10-30 22:47:04 +0100

    Added started window of Tkinter

---
+++ main.py
@@ -0,0 +1,8 @@
+from modules.tkinter_app import MainCodeOfApp
+import tkinter as tk
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    MainCodeOfApp(root)
+    root.mainloop()
---
+++ modules/tkinter_app.py
@@ -0,0 +1,10 @@
+
+class MainCodeOfApp:
+    def __init__(self, root):
+        """
+
+        @rtype: object
+        """
+        self.root = root
+
+
